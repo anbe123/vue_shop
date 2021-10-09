20496a4378ec5e47ddd3528c499a29535a38a883
zhfsolomon <zhfsolomon@gmail.com>
2021-10-09 16:34:42 +0800

    *

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+// 完整引入
+// 引入ElementUI的全部样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入ElementUI 组件库
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
